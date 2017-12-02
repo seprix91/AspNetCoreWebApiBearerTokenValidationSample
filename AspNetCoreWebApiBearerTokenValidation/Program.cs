316b0b191af1b6a3726d1c90009e6c88ed204943
aravs003 <santhosh.x.aravalli.-Nd@disney.com>
2017-12-02 14:49:43 -0800

    Add project files.

---
+++ AspNetCoreWebApiBearerTokenValidation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspNetCoreWebApiBearerTokenValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
